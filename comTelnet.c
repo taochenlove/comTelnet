ea862f5fe8bd4913a746d3395436df64864262cb
mlhl <447181856@qq.com>
2019-06-26 17:36:50 +0800

    add mult user and mult dev support

--- comTelnet.c
+++ comTelnet.c
@@ -10,12 +10,13 @@
  */
 
 
-#	include <sys/socket.h>
-#	include <netinet/in.h>
-#	include <arpa/inet.h>
-#	include <netdb.h>
-#	include <poll.h>
-#	include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <poll.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #	define SOCKET int
 
@@ -25,39 +26,137 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <fcntl.h>
 
 #include "libtelnet.h"
 #include "serial.h"
 
-#define MAX_USERS 64
+#define MAX_USERS 12
+#define MAX_SERIAL_NUM 8
 #define LINEBUFFER_SIZE 256
 
-struct pollfd pfd[MAX_USERS + 1+1];
+#define NEED_FD_MAX MAX_USERS*MAX_SERIAL_NUM + 2*MAX_SERIAL_NUM
+
+#define DEV_NAME_LENGTH 32
 
 static const telnet_telopt_t telopts[] = {
 	{ TELNET_TELOPT_COMPRESS2,	TELNET_WILL, TELNET_DONT },
 	{ -1, 0, 0 }
 };
 
-struct user_t {
+typedef struct user {
 	char *name;
 	SOCKET sock;
 	telnet_t *telnet;
 	char linebuf[256];
 	int linepos;
+	struct sockaddr_in sock_in;
+} user_t;
+
+typedef struct serial {
+	int valid;
+	int baudrate; /*波特率 */
+	char dev_name[DEV_NAME_LENGTH]; /*串口设备名/dev/ttyUSB* */
+	int dev_fd; /*打开的设备文件 */
+	int listen_fd; /*telnet监听 */
+	int listen_port; /*telnet监听端口 */
+	int users_num;
+	user_t users[MAX_USERS]; /*连接的用户 */
+}serial_t;
+
+typedef struct com_telnet {
+	serial_t serial[MAX_SERIAL_NUM];	
+}com_telnet_t;
+
+static com_telnet_t com_mng;
+static com_telnet_t *p_com_mng = &com_mng;
+
+struct option longopts[] = {
+	{"daemon", no_argument, NULL, 'd'},
+	{"telnet_port", required_argument, NULL, 'p'},
+	{"serial_dev", required_argument, NULL, 's'},
+	{"baudrate",  required_argument, NULL, 'b'},
+	{"motion", no_argument, NULL, 'm'},
+	{ 0 }
 };
 
-static struct user_t users[MAX_USERS];
+void com_usage(char *program)
+{
+	printf("Usage :%s [OPTION...]\n", program);
+	printf("-d, --daemon    Runs in daemon mode\n");
+	printf("-p, --telnet_port set telnet server port\n");
+	printf("-s, --serial_dev  set serial dev\n");
+	printf("-b, --baudrate    set baud rate\n");
+	printf("-m, --motion      self-motion\n");
 
+}
 
+serial_t * find_serial_by_listen_fd(int listen_fd)
+{
+	int i = 0;
+	for (i =0; i<MAX_SERIAL_NUM; i++) {
+		if (p_com_mng->serial[i].valid  && (p_com_mng->serial[i].listen_fd == listen_fd)) {
+			return &p_com_mng->serial[i];
+		}
+	}
+	return NULL;
+}
+
+serial_t * find_serial_by_dev_fd(int dev_fd) 
+{
+	int i = 0;
+	for (i =0; i<MAX_SERIAL_NUM; i++) {
+		if (p_com_mng->serial[i].valid  && (p_com_mng->serial[i].dev_fd == dev_fd)) {
+			return &p_com_mng->serial[i];
+		}
+	}
+	return NULL;
+}
+
+serial_t * find_serial_by_user_fd(int user_fd)
+{
+	int serial_index, client_index;
+	serial_t *p_serial = NULL;
+	for (serial_index = 0; serial_index < MAX_SERIAL_NUM; serial_index++) {
+		p_serial = &p_com_mng->serial[serial_index];
+		if (!p_serial->valid) 
+			continue;
+		
+		for (client_index=0; client_index < MAX_USERS; client_index++) {
+			if (p_serial->users[client_index].sock == user_fd)
+				return p_serial;
+		}
+	}
+	return NULL;
+}
+
+user_t * find_user_by_user_fd(int user_fd)
+{
+	int serial_index, client_index;
+	serial_t *p_serial = NULL;
+	for (serial_index = 0; serial_index < MAX_SERIAL_NUM; serial_index++) {
+		p_serial = &p_com_mng->serial[serial_index];
+		if (!p_serial->valid) 
+			continue;
+		
+		for (client_index=0; client_index < MAX_USERS; client_index++) {
+			if (p_serial->users[client_index].sock == user_fd)
+				return &p_serial->users[client_index];
+		}
+	}
+	return NULL;
+}
+
+#if 0
 static void _message(const char *from, const char *msg) {
 	int i;
 	for (i = 0; i != MAX_USERS; ++i) {
-		if (users[i].sock != -1) {
+		if (p_com_mng->users[i].sock != -1) {
 			telnet_printf(users[i].telnet, "%s: %s\n", from, msg);
 		}
 	}
 }
+#endif
 
 static void _send(SOCKET sock, const char *buffer, size_t size) {
 	int rs;
@@ -87,17 +186,22 @@ static void _send(SOCKET sock, const char *buffer, size_t size) {
 }
 
 
-static void _input(struct user_t *user, const char *buffer,
-		size_t size) {
+static void _input(struct user *user, const char *buffer, size_t size) 
+{
 	unsigned int i;
+	serial_t *p_serial  = NULL;
+	p_serial = find_serial_by_user_fd(user->sock);
+	if (p_serial ==NULL)
+		return;
+
 	for (i = 0; i != size; ++i) {
-		serialWriteChar(pfd[MAX_USERS+1].fd, buffer[i]);
+		serialWriteChar(p_serial->dev_fd, buffer[i]);
 	}
 }
 
 static void _event_handler(telnet_t *telnet, telnet_event_t *ev,
 		void *user_data) {
-	struct user_t *user = (struct user_t*)user_data;
+	struct user *user = (struct user*)user_data;
 
 	switch (ev->type) {
 	/* data received */
@@ -117,11 +221,13 @@ static void _event_handler(telnet_t *telnet, telnet_event_t *ev,
 	case TELNET_EV_ERROR:
 		close(user->sock);
 		user->sock = -1;
+	#if 0
 		if (user->name != 0) {
 			_message(user->name, "** HAS HAD AN ERROR **");
 			free(user->name);
 			user->name = 0;
 		}
+	#endif
 		telnet_free(user->telnet);
 		break;
 		
@@ -131,6 +237,53 @@ static void _event_handler(telnet_t *telnet, telnet_event_t *ev,
 		break;
 	}
 }
+
+void set_fd_noblock(int fd)
+{
+	int val;
+	val = fcntl(fd, F_GETFL, 0);
+	fcntl(fd, F_SETFL, val | O_NONBLOCK);
+	return;
+}
+
+int init_listen_socket(int listen_port)
+{
+	int listen_sock;
+	int rs;
+	struct sockaddr_in addr;
+
+	/* create listening socket */
+	if ((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "socket() failed: %s\n", strerror(errno));
+		return -1;
+	}
+
+	/* reuse address option */
+	rs = 1;
+	setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (char*)&rs, sizeof(rs));
+
+	/* bind to listening addr/port */
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(listen_port);
+	if (bind(listen_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+		fprintf(stderr, "bind() failed: %s\n", strerror(errno));
+		close(listen_sock);
+		return -1;
+	}
+	set_fd_noblock(listen_sock);
+	/* listen for clients */
+	if (listen(listen_sock, 5) == -1) {
+		fprintf(stderr, "listen() failed: %s\n", strerror(errno));
+		close(listen_sock);
+		return -1;
+	}
+
+	return listen_sock;
+
+}
+
 void exithandler() {
    // serialClose(fd);
    return;
@@ -147,157 +300,234 @@ int main(int argc, char **argv) {
 	struct sockaddr_in addr;
 	socklen_t addrlen;
 	int fd;
+	struct pollfd pfd[NEED_FD_MAX];
 
+	char *p;
+	char *progname_l;
+	int daemon_mode = 0;
+	int telnet_port = 100;
+	int baudrate = 115200; /*default 115200*/
+	int self_motion = 0;
+	char **self_serial = NULL;
+	serial_t *p_serial = NULL;
+	user_t *p_user = NULL;
+	int client_index, serial_index;
 
-	/* check usage */
-	if (argc != 3) {
-		fprintf(stderr, "Usage:\n %s <port> </dev/ttyUSB*>\n", argv[0]);
-		return 1;
+	memset(p_com_mng, 0, sizeof(struct com_telnet));
+
+	progname_l = ((p = strrchr (argv[0], '/')) ? ++p : argv[0]);
+
+	if (argc == 1) {
+		com_usage(progname_l);
+		return 0;
 	}
 
+	while (1) {
+		int opt = getopt_long(argc, argv, "dp:s:b:m", longopts, 0);
+		if (opt == EOF)
+			break;
+		switch (opt) {
+			case 0:
+				break;
+			case 'd':
+				daemon_mode = 1;
+				break;
+			case 'p':
+				telnet_port = atoi(optarg);
+				break;
+			case 's':
+				strncpy(serial_dev, optarg, 32);
+				break;
+			case 'b':
+				baudrate = atoi(optarg);
+				break;
+			case 'm':
+				self_motion = 1;
+				break;
+			default:
+				com_usage(progname_l);
+				break;
+		}
+	}
 
 	/* initialize data structures */
 	memset(&pfd, 0, sizeof(pfd));
-	memset(users, 0, sizeof(users));
-	for (i = 0; i != MAX_USERS; ++i)
-		users[i].sock = -1;
+	for (i = 0; i != NEED_FD_MAX; ++i)
+		pfd[i].fd = -1;
 
-	/* parse listening port */
-	listen_port = (short)strtol(argv[1], 0, 10);
-	strncpy(serial_dev, argv[2], 32);
 
-	fd = serialOpen(serial_dev, 115200);
-	if (fd < 0) {
-		fprintf(stderr, "serialOpen failed, fd=%d\n", fd);
-		return -1;
+	if (self_motion) {
+		self_serial = getSerialPorts();
+		for (i=0; self_serial[i] != NULL; i++) {
+			int serial_port;
+			fd = serialOpen(self_serial[i], baudrate);
+			if (fd < 0) {
+				fprintf(stderr, "serialOpen failed, fd=%d\n", fd);
+				return -1;
+			}
+			p = strrchr (self_serial[i], 'B'); ++p;
+			serial_port = (atoi(p) +1) * 100;
+			p_com_mng->serial[i].valid = 1;
+			p_com_mng->serial[i].baudrate = baudrate;
+			p_com_mng->serial[i].listen_port = serial_port;
+			p_com_mng->serial[i].dev_fd =fd;
+			strncpy(p_com_mng->serial[i].dev_name, self_serial[i], DEV_NAME_LENGTH);
+
+			pfd[MAX_SERIAL_NUM+i].fd = fd;
+			pfd[MAX_SERIAL_NUM+i].events = POLLIN;
+
+			
+			fd = init_listen_socket(serial_port);
+			p_com_mng->serial[i].listen_fd = fd;
+			pfd[i].fd = fd;
+			pfd[i].events = POLLIN;
+			printf("open dev [%s] listen on port [%d]\n", self_serial[i], serial_port);
+			
+		}
+	} else {
+		fd = serialOpen(serial_dev, baudrate);
+		if (fd < 0) {
+			fprintf(stderr, "serialOpen failed, fd=%d\n", fd);
+			return -1;
+		}
+		p_com_mng->serial[0].valid = 1;
+		p_com_mng->serial[0].baudrate = baudrate;
+		p_com_mng->serial[0].dev_fd =fd;
+		strncpy(p_com_mng->serial[0].dev_name, serial_dev, DEV_NAME_LENGTH);
+		pfd[MAX_SERIAL_NUM+1].fd = fd;
+		pfd[MAX_SERIAL_NUM+1].events = POLLIN;
+		fd = init_listen_socket(telnet_port);
+		p_com_mng->serial[0].listen_fd = fd;
+		p_com_mng->serial[0].listen_port = telnet_port;
+		pfd[0].fd = fd;
+		pfd[0].events = POLLIN;
+		fd = init_listen_socket(telnet_port);
+
+		printf("open dev [%s] listen on port [%d]\n", serial_dev, telnet_port);
 	}
+	
 	atexit(exithandler);
-	/* create listening socket */
-	if ((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		fprintf(stderr, "socket() failed: %s\n", strerror(errno));
-		return 1;
+
+	for (serial_index = 0; serial_index < MAX_SERIAL_NUM; serial_index++) {
+		for (client_index = 0; client_index < MAX_USERS; client_index++) {
+			p_com_mng->serial[serial_index].users[client_index].sock = -1;
+		}
 	}
-
-	/* reuse address option */
-	rs = 1;
-	setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (char*)&rs, sizeof(rs));
-
-	/* bind to listening addr/port */
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(listen_port);
-	if (bind(listen_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-		fprintf(stderr, "bind() failed: %s\n", strerror(errno));
-		close(listen_sock);
-		return 1;
-	}
-
-	/* listen for clients */
-	if (listen(listen_sock, 5) == -1) {
-		fprintf(stderr, "listen() failed: %s\n", strerror(errno));
-		close(listen_sock);
-		return 1;
-	}
-
-	printf("LISTENING ON PORT %d\n", listen_port);
-
-	/* initialize listening descriptors */
-	pfd[MAX_USERS].fd = listen_sock;
-	pfd[MAX_USERS].events = POLLIN;
-
-	pfd[MAX_USERS+1].fd = fd;
-	pfd[MAX_USERS+1].events = POLLIN;
-
+	
 	/* loop for ever */
 	for (;;) {
-		/* prepare for poll */
-		for (i = 0; i != MAX_USERS; ++i) {
-			if (users[i].sock != -1) {
-				pfd[i].fd = users[i].sock;
-				pfd[i].events = POLLIN;
-			} else {
-				pfd[i].fd = -1;
-				pfd[i].events = 0;
+		int listen_index = 0;
+		
+		for (serial_index = 0; serial_index < MAX_SERIAL_NUM; serial_index++) {
+
+			for (client_index = 0; client_index < MAX_USERS; client_index++) {
+				p_user = &p_com_mng->serial[serial_index].users[client_index];
+				if (p_user->sock != -1) {
+					pfd[2*MAX_SERIAL_NUM+serial_index*MAX_USERS+client_index].fd = p_user->sock;
+					pfd[2*MAX_SERIAL_NUM+serial_index*MAX_USERS+client_index].events = POLLIN;
+				} else {
+					pfd[2*MAX_SERIAL_NUM+serial_index*MAX_USERS+client_index].fd = -1;
+					pfd[2*MAX_SERIAL_NUM+serial_index*MAX_USERS+client_index].events = 0;
+				}
 			}
 		}
 
 		/* poll */
-		rs = poll(pfd, MAX_USERS + 1+1, -1);
+		rs = poll(pfd, NEED_FD_MAX, -1);
 		if (rs == -1 && errno != EINTR) {
 			fprintf(stderr, "poll() failed: %s\n", strerror(errno));
 			close(listen_sock);
 			return 1;
 		}
 
-		/* new connection */
-		if (pfd[MAX_USERS].revents & POLLIN) {
-			/* acept the sock */
-			addrlen = sizeof(addr);
-			if ((client_sock = accept(listen_sock, (struct sockaddr *)&addr,
-					&addrlen)) == -1) {
-				fprintf(stderr, "accept() failed: %s\n", strerror(errno));
-				return 1;
-			}
-
-			printf("Connection received.\n");
-
-			/* find a free user */
-			for (i = 0; i != MAX_USERS; ++i)
-				if (users[i].sock == -1)
-					break;
-			if (i == MAX_USERS) {
-				printf("  rejected (too many users)\n");
-				_send(client_sock, "Too many users.\r\n", 14);
-				close(client_sock);
-			}
-
-			/* init, welcome */
-			users[i].sock = client_sock;
-			users[i].telnet = telnet_init(telopts, _event_handler, TELNET_FLAG_NVT_EOL,
-					&users[i]);
-			telnet_negotiate(users[i].telnet, TELNET_WILL,
-					TELNET_TELOPT_COMPRESS2);
-
-	//		telnet_printf(users[i].telnet, "Enter name: ");
-
-			telnet_negotiate(users[i].telnet, TELNET_WILL, TELNET_TELOPT_ECHO);
-			telnet_negotiate(users[i].telnet, TELNET_WONT, TELNET_TELOPT_LINEMODE);
-			telnet_negotiate(users[i].telnet, TELNET_WILL, TELNET_TELOPT_SGA);
-
-		}
-
-		if (pfd[MAX_USERS + 1].revents & POLLIN) {
-			char c;
-			serialReadChar(fd, &c);
-			for (i = 0; i< MAX_USERS; i++) {
-				if (users[i].telnet != NULL) {
-			//		printf("%c", c);
-					telnet_printf(users[i].telnet, "%c", c);
+		for (listen_index = 0; listen_index < MAX_SERIAL_NUM; listen_index++) {
+			char ip[32];
+			/* new connection */
+			if (pfd[listen_index].revents & POLLIN) {
+				/* acept the sock */
+				addrlen = sizeof(addr);
+				if ((client_sock = accept(pfd[listen_index].fd, (struct sockaddr *)&addr,
+						&addrlen)) == -1) {
+					fprintf(stderr, "accept() failed: %s\n", strerror(errno));
+					
+					if (errno == EAGAIN)
+						continue;
+					return 1;
 				}
-			
+
+				set_fd_noblock(client_sock);
+
+				
+				
+
+				/* find a free user */
+				p_serial = find_serial_by_listen_fd(pfd[listen_index].fd); 
+				if (p_serial == NULL) {
+					printf("ERROR!");
+					return -1;
+				}
+
+				for (i=0; i < MAX_USERS; i++) {
+					if (p_serial->users[i].sock == -1) {
+						p_serial->users[i].sock = client_sock;
+						p_serial->users[i].telnet = telnet_init(telopts, _event_handler, TELNET_FLAG_NVT_EOL, &p_serial->users[i]);
+						telnet_negotiate(p_serial->users[i].telnet, TELNET_WILL, TELNET_TELOPT_COMPRESS2);
+						telnet_negotiate(p_serial->users[i].telnet, TELNET_WILL, TELNET_TELOPT_ECHO);
+						telnet_negotiate(p_serial->users[i].telnet, TELNET_WONT, TELNET_TELOPT_LINEMODE);
+						telnet_negotiate(p_serial->users[i].telnet, TELNET_WILL, TELNET_TELOPT_SGA);
+						memcpy(&p_serial->users[i].sock_in, &addr, sizeof(addr));
+						p_serial->users_num++;
+						break;
+					}					
+				}
+
+				printf("[%s][%d] Connecte user [%s][%u]. current user number [%d]\n", p_serial->dev_name, p_serial->listen_port, inet_ntop(addr.sin_family, &(addr.sin_addr), ip, 16), ntohs(addr.sin_port), p_serial->users_num);
+				
+				if (i == MAX_USERS) {
+					printf("  rejected (too many users)\n");
+					_send(client_sock, "Too many users.\r\n", 14);
+					close(client_sock);
+				}
 			}
+
 		}
 
-		/* read from client */
-		for (i = 0; i != MAX_USERS; ++i) {
-			/* skip users that aren't actually connected */
-			if (users[i].sock == -1)
-				continue;
-
+		for (i=MAX_SERIAL_NUM; i< 2*MAX_SERIAL_NUM; i++) {
 			if (pfd[i].revents & POLLIN) {
-				if ((rs = recv(users[i].sock, buffer, sizeof(buffer), 0)) > 0) {
-					telnet_recv(users[i].telnet, buffer, rs);
-				} else if (rs == 0) {
-					printf("Connection closed.\n");
-					close(users[i].sock);
-					if (users[i].name != 0) {
-						_message(users[i].name, "** HAS DISCONNECTED **");
-						free(users[i].name);
-						users[i].name = 0;
+				char c;
+				serialReadChar(pfd[i].fd, &c);
+				p_serial = find_serial_by_dev_fd(pfd[i].fd);
+				if (p_serial == NULL) {
+					printf("by dev_fd find failed!\n");
+					return -1;
+				}
+				for (i = 0; i< MAX_USERS; i++) {
+					if (p_serial->users[i].telnet != NULL) {
+						telnet_printf(p_serial->users[i].telnet, "%c", c);
 					}
-					telnet_free(users[i].telnet);
-					users[i].sock = -1;
+				
+				}
+			}
+		}			
+		
+		/* read from client */
+		for (i = 2*MAX_SERIAL_NUM; i < NEED_FD_MAX; ++i) {
+			if (pfd[i].revents & POLLIN) {
+				p_serial = find_serial_by_user_fd(pfd[i].fd);
+				p_user = find_user_by_user_fd(pfd[i].fd);
+				if (p_user->sock == -1)
+					continue;
+				
+
+				if ((rs = recv(pfd[i].fd, buffer, sizeof(buffer), 0)) > 0) {
+					telnet_recv(p_user->telnet, buffer, rs);
+				} else if (rs == 0) {
+					char ip[32];
+					close(p_user->sock);
+					telnet_free(p_user->telnet);
+					p_user->sock = -1;
+					p_serial->users_num--;
+					printf("[%s][%d] leave user [%s][%u]. current user number [%d]\n", p_serial->dev_name, p_serial->listen_port, inet_ntop(p_user->sock_in.sin_family, &(p_user->sock_in.sin_addr), ip, 16), ntohs(p_user->sock_in.sin_port), p_serial->users_num);
 					break;
 				} else if (errno != EINTR) {
 					fprintf(stderr, "recv(client) failed: %s\n",
